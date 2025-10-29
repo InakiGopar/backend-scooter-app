19b346772064798ff0e336c27ebc968ed52ca688
Iñaki <tatigop09@gmail.com>
2025-10-29 16:26:53 -0300

    first commit

---
+++ trip-service/src/test/java/ar/edu/unicen/tripservice/TripServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unicen.tripservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
