4efde1e515183f8ad0e8e0126c7555794e31d343
Iñaki <tatigop09@gmail.com>
2025-11-09 12:26:09 -0300

    add basic CRUD in Fee and refactor the attribute feeRepository to feeService on TripService

--- trip-service/src/main/java/ar/edu/unicen/tripservice/application/services/FeeService.java
+++ trip-service/src/main/java/ar/edu/unicen/tripservice/application/services/FeeService.java
@@ -1,6 +1,10 @@
 package ar.edu.unicen.tripservice.application.services;
 
 import ar.edu.unicen.tripservice.application.repositories.FeeRepository;
+import ar.edu.unicen.tripservice.domain.dtos.request.fee.FeeRequestDTO;
+import ar.edu.unicen.tripservice.domain.dtos.response.fee.FeeResponseDTO;
+import ar.edu.unicen.tripservice.domain.entities.Fee;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +12,48 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FeeService {
     private final FeeRepository feeRepository;
+
+    public FeeResponseDTO createFee(FeeRequestDTO request) {
+        Fee fee = request.toEntity();
+        feeRepository.save(fee);
+        return FeeResponseDTO.toDTO(fee);
+    }
+
+    public FeeResponseDTO updateFee( String feeId, FeeRequestDTO request) {
+        Fee fee = feeRepository.findById(feeId).orElseThrow(
+                ()-> new EntityNotFoundException("Fee not found with id " + feeId)
+        );
+
+        fee.setStartDate(request.startDate());
+        fee.setEndDate(request.endDate());
+        fee.setPricePerHour(request.pricePerHour());
+        fee.setExtraHourFee(request.extraHourFee());
+
+        feeRepository.save(fee);
+        return FeeResponseDTO.toDTO(fee);
+    }
+
+    public FeeResponseDTO updateFeeExtraHour( String feeId, FeeRequestDTO request) {
+        Fee fee = feeRepository.findById(feeId).orElseThrow(
+                ()-> new EntityNotFoundException("Fee not found with id " + feeId)
+        );
+        fee.setExtraHourFee(request.extraHourFee());
+        feeRepository.save(fee);
+        return FeeResponseDTO.toDTO(fee);
+    }
+
+    public FeeResponseDTO getFeeById( String feeId ) {
+        Fee fee = feeRepository.findById(feeId).orElseThrow(
+                ()-> new EntityNotFoundException("Fee not found with id " + feeId)
+        );
+        return FeeResponseDTO.toDTO(fee);
+    }
+
+    public FeeResponseDTO deleteFee(String feeId) {
+        Fee fee = feeRepository.findById(feeId).orElseThrow(
+                ()-> new EntityNotFoundException("Fee not found with id " + feeId)
+        );
+        feeRepository.delete(fee);
+        return FeeResponseDTO.toDTO(fee);
+    }
 }
--- trip-service/src/main/java/ar/edu/unicen/tripservice/infrastructure/controllers/FeeController.java
+++ trip-service/src/main/java/ar/edu/unicen/tripservice/infrastructure/controllers/FeeController.java
@@ -2,7 +2,9 @@ package ar.edu.unicen.tripservice.infrastructure.controllers;
 
 import ar.edu.unicen.tripservice.application.services.FeeService;
 import ar.edu.unicen.tripservice.application.services.TripService;
+import ar.edu.unicen.tripservice.domain.dtos.request.fee.FeeRequestDTO;
 import ar.edu.unicen.tripservice.domain.dtos.request.trip.TripRequestDTO;
+import ar.edu.unicen.tripservice.domain.dtos.response.fee.FeeResponseDTO;
 import ar.edu.unicen.tripservice.domain.dtos.response.trip.TripResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,5 +17,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FeeController {
     private final FeeService feeService;
+
+    @PostMapping
+    public ResponseEntity<FeeResponseDTO> createFee(@RequestBody FeeRequestDTO request) {
+        return ResponseEntity.status(201).body(feeService.createFee(request));
+    }
+
+    @PutMapping("/{feeId}")
+    public ResponseEntity<FeeResponseDTO> updateFee(@PathVariable String feeId, @RequestBody FeeRequestDTO request) {
+        return ResponseEntity.ok(feeService.updateFee(feeId, request));
+    }
+
+    @PatchMapping("/{feeId}/extraHour")
+    public ResponseEntity<FeeResponseDTO> updateExtraHour(@PathVariable String feeId, @RequestBody FeeRequestDTO request) {
+        return ResponseEntity.ok(feeService.updateFeeExtraHour(feeId, request));
+    }
+
+    @GetMapping
+    public ResponseEntity<FeeResponseDTO> getFeeById(@RequestParam String feeId) {
+        return ResponseEntity.ok(feeService.getFeeById(feeId));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteFee(@RequestParam String feeId) {
+        feeService.deleteFee(feeId);
+        return ResponseEntity.noContent().build();
+    }
 }
 
