c2acb6ee184397b41bf94ba391dfec09a363f16d
Lautaro <lautaroouvrard@gmail.com>
2025-11-09 01:11:47 -0300

    add calculateTotalPrice, startPause and endPause methods; refactor Trip to use date fields for start, end, and pauses

--- trip-service/src/main/java/ar/edu/unicen/tripservice/application/repositories/FeeRepository.java
+++ trip-service/src/main/java/ar/edu/unicen/tripservice/application/repositories/FeeRepository.java
@@ -2,8 +2,9 @@ package ar.edu.unicen.tripservice.application.repositories;
 
 import ar.edu.unicen.tripservice.domain.entities.Fee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeeRepository extends JpaRepository<Fee, Long> {
+public interface FeeRepository extends MongoRepository<Fee, String> {
 }
