660781e3d21f7f330d11edddf3d2c1903993273d
Lautaro <lautaroouvrard@gmail.com>
2025-11-24 15:07:40 -0300

    add: unversioned files

---
+++ auth-service/src/main/java/ar/edu/unicen/authservice/AuthServiceApplication.java
@@ -0,0 +1,17 @@
+package ar.edu.unicen.authservice;
+
+import ar.edu.unicen.authservice.infrastructure.config.JwtProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
